ed63f2ea4c8bbe7e59ffdefe406f04208e839173
Claudio Cicconetti <ccicconetti@mbigroup.it>
2019-06-06 14:54:58 +0200

    Update boilerplat

--- Test/testqueue.cpp
+++ Test/testqueue.cpp
@@ -6,7 +6,7 @@
 |_______|__|__/__/   /__/
 
 Licensed under the MIT License <http://opensource.org/licenses/MIT>.
-Copyright (c) 2018 Claudio Cicconetti <https://about.me/ccicconetti>
+Copyright (c) 2019 Claudio Cicconetti https://ccicconetti.github.io/
 
 Permission is hereby  granted, free of charge, to any  person obtaining a copy
 of this software and associated  documentation files (the "Software"), to deal
