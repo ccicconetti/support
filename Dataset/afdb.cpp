1076a20010e8dd9ff0c45ddf5bda2081a842ddc3
Claudio Cicconetti <cicconetti@gmail.com>
2022-02-07 23:09:47 +0100

    Work on afdb

--- Dataset/afdb.cpp
+++ Dataset/afdb.cpp
@@ -29,19 +29,121 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+//
+// https://github.com/Azure/AzurePublicDataset/blob/master/AzureFunctionsBlobDataset2020.md
+//
+
 #include "Support/glograii.h"
+#include "Support/split.h"
 
 #include <boost/program_options.hpp>
 
+#include <exception>
 #include <glog/logging.h>
 
+#include <cassert>
+#include <deque>
 #include <fstream>
+#include <iostream>
+#include <map>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 namespace po = boost::program_options;
 namespace us = uiiit::support;
 
+struct Row {
+  Row(const std::string& aRow) {
+    auto myTokens = us::split<std::vector<std::string>>(aRow, ",");
+    if (myTokens.size() != 11) {
+      throw std::runtime_error("Wrong number of elements in row: " + aRow);
+    }
+    theTimestamp = std::stod(myTokens[0]);
+    theRegion.swap(myTokens[1]);
+    theUser.swap(myTokens[2]);
+    theApp.swap(myTokens[3]);
+    theFunction.swap(myTokens[4]);
+    theBlob.swap(myTokens[5]);
+    theBlobType.swap(myTokens[6]);
+    theBlobVersion.swap(myTokens[7]);
+    theBlobSize = std::stoull(myTokens[8]);
+    if (myTokens[9] == "True" and myTokens[10].size() >= 5 and
+        myTokens[10].substr(0, 5) == "False") {
+      theWrite = false;
+    } else if (myTokens[9] == "False" and myTokens[10].size() >= 4 and
+               myTokens[10].substr(0, 4) == "True") {
+      theWrite = true;
+    } else {
+      throw std::runtime_error("Invalid read/write flags in row: " + aRow);
+    }
+  }
+
+  double      theTimestamp;
+  std::string theRegion;
+  std::string theUser;
+  std::string theApp;
+  std::string theFunction;
+  std::string theBlob;
+  std::string theBlobType;
+  std::string theBlobVersion;
+  std::size_t theBlobSize;
+  bool        theWrite;
+};
+
+std::deque<Row> loadDataset(std::istream& aStream, const bool aWithHeader) {
+  std::size_t myRowId = 0;
+  std::string myLine;
+  if (aWithHeader) {
+    std::getline(aStream, myLine);
+  }
+  if (myLine.empty()) {
+    throw std::runtime_error("Invalid empty header");
+  }
+  std::deque<Row> ret;
+  while (aStream) {
+    ++myRowId;
+    try {
+      std::getline(aStream, myLine);
+      if (myLine.empty()) {
+        break;
+      }
+      ret.emplace_back(Row(myLine));
+    } catch (const std::exception& aErr) {
+      LOG(ERROR) << "error reading line " << myRowId << ": " << aErr.what();
+    }
+  }
+  return ret;
+}
+
+std::unordered_map<std::string, std::vector<double>>
+readWritePeriods(const std::deque<Row>& aDataset) {
+  std::unordered_map<std::string, std::vector<double>> ret;
+
+  // key: user id + app id
+  // value: 0: last timestamp
+  //        1: last write flag
+  std::unordered_map<std::string, std::tuple<double, bool>> myLast;
+  for (const auto& myRow : aDataset) {
+    const auto myKey = myRow.theUser + "|" + myRow.theApp;
+    auto       res   = myLast.emplace(
+        myKey, std::make_tuple(myRow.theTimestamp, myRow.theWrite));
+    if (not res.second) {
+      if (std::get<1>(res.first->second) != myRow.theWrite) {
+        ret[myKey].emplace_back(myRow.theTimestamp -
+                                std::get<0>(res.first->second));
+        std::get<0>(res.first->second) = myRow.theTimestamp;
+        std::get<1>(res.first->second) = myRow.theWrite;
+      }
+    }
+  }
+
+  return ret;
+}
+
 int main(int argc, char* argv[]) {
-  uiiit::support::GlogRaii myGlogRaii(argv[0]);
+  us::GlogRaii myGlogRaii(argv[0]);
 
   std::string myDatasetFilename;
 
@@ -49,7 +151,7 @@ int main(int argc, char* argv[]) {
   // clang-format off
   myDesc.add_options()
     ("help,h", "produce help message")
-    ("output",
+    ("input-dataset",
      po::value<std::string>(&myDatasetFilename)->default_value(""),
      "Input dataset file name.")
     ;
@@ -65,11 +167,13 @@ int main(int argc, char* argv[]) {
       return EXIT_FAILURE;
     }
 
-    std::ifstream myDataset(myDatasetFilename);
-    if (not myDataset) {
+    std::ifstream myDatasetStream(myDatasetFilename);
+    if (not myDatasetStream) {
       throw std::runtime_error("could not open dataset file for reading: " +
                                myDatasetFilename);
     }
+
+    auto myDataset = loadDataset(myDatasetStream, true);
 
     return EXIT_SUCCESS;
   } catch (const std::exception& aErr) {
